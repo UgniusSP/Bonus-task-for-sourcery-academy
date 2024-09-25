7a2bf5355848255543365f191559814bfe9d19c1
ugnius.spokavicius <uspokavicius@gmail.com>
2024-09-25 22:23:05 +0300

    bugfix

--- src/main/java/com/ugnius/task/controllers/BookController.java
+++ src/main/java/com/ugnius/task/controllers/BookController.java
@@ -3,11 +3,10 @@ package com.ugnius.task.controllers;
 import com.ugnius.task.models.Book;
 import com.ugnius.task.services.BookService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/books")
@@ -18,6 +17,9 @@ public class BookController {
 
     @PostMapping("/{id}/rate")
     public ResponseEntity<Book> rateBook(@PathVariable Long id, @RequestParam double rating) {
+        if (rating < 0 || rating > 5) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
         return ResponseEntity.ok(bookService.rateBook(id, rating));
     }
 
