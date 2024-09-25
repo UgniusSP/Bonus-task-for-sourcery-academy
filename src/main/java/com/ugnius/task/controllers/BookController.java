5cdbe642c2249d6577675ed5f9d96fca4bd9dd27
ugnius.spokavicius <uspokavicius@gmail.com>
2024-09-25 22:52:11 +0300

    added filter

--- src/main/java/com/ugnius/task/controllers/BookController.java
+++ src/main/java/com/ugnius/task/controllers/BookController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 
 @RestController
 @RequestMapping("/api/books")
@@ -14,6 +16,14 @@ import org.springframework.web.bind.annotation.*;
 public class BookController {
 
     private final BookService bookService;
+
+    @GetMapping("/filter")
+    public List<Book> getAllBooks(@RequestParam(required = false) String title,
+                                  @RequestParam(required = false) String author,
+                                  @RequestParam(required = false) Integer year,
+                                  @RequestParam(required = false) Double rating) {
+        return bookService.filterBooks(title, author, year, rating);
+    }
 
     @PostMapping("/{id}/rate")
     public ResponseEntity<Book> rateBook(@PathVariable Long id, @RequestParam double rating) {
--- src/main/java/com/ugnius/task/services/BookService.java
+++ src/main/java/com/ugnius/task/services/BookService.java
@@ -5,11 +5,21 @@ import com.ugnius.task.repositories.BookRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class BookService {
 
     private final BookRepository bookRepository;
+
+    public List<Book> filterBooks(String title, String author, Integer year, Double rating) {
+        if (title != null) return bookRepository.findByTitle(title);
+        if (author != null) return bookRepository.findByAuthor(author);
+        if (year != null) return bookRepository.findByPublicationYear(year);
+        if (rating != null) return bookRepository.findByRating(rating);
+        return bookRepository.findAll();
+    }
 
     public Book rateBook(Long id, Double rating) {
         Book book = bookRepository.findById(id).orElseThrow(() -> new RuntimeException("Book not found"));
