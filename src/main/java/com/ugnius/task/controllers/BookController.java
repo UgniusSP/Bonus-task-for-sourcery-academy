ff1dc0eaeed8f18c8c178429e694aa17929555e8
ugnius.spokavicius <uspokavicius@gmail.com>
2024-09-25 22:25:00 +0300

    changed min rating value from 0 to 1

--- src/main/java/com/ugnius/task/controllers/BookController.java
+++ src/main/java/com/ugnius/task/controllers/BookController.java
@@ -17,7 +17,7 @@ public class BookController {
 
     @PostMapping("/{id}/rate")
     public ResponseEntity<Book> rateBook(@PathVariable Long id, @RequestParam double rating) {
-        if (rating < 0 || rating > 5) {
+        if (rating < 1 || rating > 5) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         }
         return ResponseEntity.ok(bookService.rateBook(id, rating));
